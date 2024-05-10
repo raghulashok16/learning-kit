d159907f0baf8392d4d08a9c0227e7621df0b373
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-05-10 16:27:30 +0530

    Update 3_Javascript.js

--- 3_Javascript.js
+++ 3_Javascript.js
@@ -7,9 +7,9 @@
 // [Null - also means empty value]
 // [Symbol (ES2015) - values that is unique and cannot be changed - not usefull for now]
 // [BigInt (ES2020) - larger integers than Number type can hold]
-// [javascript has dynamic typing - we do not have to manually define the data type of the value stored in a variable, instead datatypes are defined automatically]
-
-// datatypes();
+// [javascript has dynamic typing]
+// [we do not have to manually define the data type of the value stored in a variable]
+// [instead datatypes are defined automatically]
 function datatypes() {
     console.log(typeof ('javascript'));
     console.log(typeof ('A'));
@@ -26,11 +26,16 @@ function datatypes() {
     console.log(typeof (null));
     // [typeof null is object and it is not corrected for leagacy reasons]
 };
+// datatypes();
 
 /* ------------------------------ LET-CONST-VAR ----------------------------- */
-// [The let keyword creates block-scoped variables, which means they are only accessible within a particular block of code]
+// [The let and const keyword was introduced in the later version of JavaScript known as ES6(ES2015)]
+// [And it's the preferred way to declare variables]
+// [let - block-scoped. let does not allow to redeclare variables. Hoisting does not occur in let]
+// [var - function scoped. var allows to redeclare variables. Hoisting occurs in var]
 
-// let_declarations();
+/* -------------------------------------------------------------------------- */
+// [The let keyword creates block-scoped variables, which means they are only accessible within a particular block of code]
 function let_declarations() {
     {
         // block of code
@@ -42,10 +47,10 @@ function let_declarations() {
     // can't be accessed here
     console.log(name);
 };
+// let_declarations();
 
+/* -------------------------------------------------------------------------- */
 // [The const keyword creates constant variables that cannot be changed after declaration]
-
-// const_declarations();
 function const_declarations() {
     // declare variable with const
     const fruit = "Apple";
@@ -55,16 +60,12 @@ function const_declarations() {
     fruit = "Banana";
     console.log(fruit);
 }
+// const_declarations();
 
-// [The let and const keyword was introduced in the later version of JavaScript known as ES6(ES2015). And it's the preferred way to declare variables]
-// [let - block-scoped. let does not allow to redeclare variables. Hoisting does not occur in let]
-// [var - function scoped. var allows to redeclare variables. Hoisting occurs in var]
-
-
+/* -------------------------------------------------------------------------- */
 // [The variable declared inside a function with var can be used anywhere within a function]
-// [In the above program, the variable a is declared with var. The variable a can be used anywhere inside the function greet]
-
-// var_keyword(); // hello
+// [In the above program, the variable a is declared with var]
+// [The variable a can be used anywhere inside the function]
 // variable a cannot be used here
 function var_keyword() {
     // variable a can be used here
@@ -72,11 +73,10 @@ function var_keyword() {
     console.log(a);
 }
 // variable a cannot be used here
+// var_keyword(); // hello
 
-
+/* -------------------------------------------------------------------------- */
 // [The variable declared with let can only be accessed inside a block of code]
-
-// let_keyword();
 // variable a cannot be used here
 function let_keyword() {
     let a = 'hello';
@@ -90,33 +90,57 @@ function let_keyword() {
     console.log(a + ' ' + b); // error
 };
 // variable a cannot be used here
+// let_keyword();
+
 
 /* ----------------------------- VARIABLE SCOPE ----------------------------- */
 // [the scope of a variable determines where it can be accessed within the code]
 // [Global Scope, Local (Function) Scope, Block-Level Scope]
 
+/* -------------------------------------------------------------------------- */
 // [When variables are declared inside a function, they have a local scope and are accessible only within that function]
-// [Here, the message variable is local to the greet() function. So, it can only be accessed within that function. That's why we get an error when we try to access it outside the greet() function. To fix this issue, we can make the message variable global]
-
-// local_scope();
+// [Here, the message variable is local to the function]
+// [So, it can only be accessed within that function]
+// [That's why we get an error when we try to access it outside the function]
+// [To fix this issue, we can make the message variable global]
 function local_scope() {
     // local variable
     var message = "Hello";
     console.log(`Local: ${message}`);
 }
 // try to access message variable
-// outside the greet() function
-console.log(`Global: ${message}`);
+// outside the function
+// console.log(`Global: ${message}`);
+// local_scope();
 
+/* -------------------------------------------------------------------------- */
 // [a variable declared outside any function or in the global scope is known as a global variable]
 // [A global variable can be accessed both inside and outside of functions]
-// [Here, we can access the message variable from outside of the greet() function. This is possible because we have created the message variable in the global scope (outside the function). Thus, message will be accessible from any scope (region) of the program]
-
-// global_scope();
+// [Here, we can access the message variable from outside of the function]
+// [This is possible because we have created the message variable in the global scope (outside the function)]
+// [Thus, message will be accessible from any scope (region) of the program]
 // declare global variable
 var message = "Hello";
 function global_scope() {
     console.log(`Local: ${message}`);
 }
 // console.log(`Global: ${message}`);
+// global_scope();
 
+/* -------------------------------------------------------------------------- */
+// [JavaScript ES6 introduced block-level scoping with the let and const keywords]
+// [Block-level variables are accessible only within the block {} they are defined in, which can be smaller than a function's scope]
+// [In this example, we have created two separate message variables]
+// [Block-Level: The variable inside the if block (visible only there)]
+// [Local-Level: The variable inside the  function]
+function block_level_scope() {
+    // declare variable in local scope
+    let message = "local";
+    if (true) {
+        // declare block-level variable
+        let message = "block-level";
+        console.log(`inner scope: ${message}`);
+    }
+    console.log(`outer scope: ${message}`);
+}
+// block_level_scope();
