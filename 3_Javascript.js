283c2609287c0e23c82609cdb7a1f9c6be3c2105
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-05-10 17:00:26 +0530

    Update 3_Javascript.js

--- 3_Javascript.js
+++ 3_Javascript.js
@@ -144,3 +144,81 @@ function block_level_scope() {
     console.log(`outer scope: ${message}`);
 }
 // block_level_scope();
+
+/* -------------------------- JAVASCRIPT OPERATORS -------------------------- */
+// [JavaScript operators are special symbols that perform operations on one or more operands (values)]
+// [an operator is a symbol or keyword that performs an operation on one or more operands, producing a result]
+
+/* -------------------------------------------------------------------------- */
+// [Arithmetic Operators: Used for basic arithmetic operations like addition, subtraction, multiplication, division, and modulus]
+// [Addition (+), Subtraction (-), Multiplication (*), Division (/), Modulus (%), Increment (++) and Decrement (--)]
+function arithmetic_operator() {
+    let x = 5;
+    // addition operator
+    console.log('Addition: x + 3 = ', x + 3);
+    // subtraction operator
+    console.log("Subtraction: x - 3 =", x - 3);
+    // multiplication operator
+    console.log("Multiplication: x * 3 =", x * 3);
+    // division operator
+    console.log("Division: x / 3 =", x / 3);
+    // remainder operator
+    console.log("Remainder: x % 3 =", x % 3);
+    // increment operator
+    console.log("Increment: ++x =", ++x);
+    // decrement operator
+    console.log("Decrement: --x =", --x);
+    // exponentiation operator
+    console.log("Exponentiation: x ** 3 =", x ** 3);
+}
+// arithmetic_operator();
+
+/* -------------------------------------------------------------------------- */
+// [Assignment Operators: Used to assign values to variables]
+// [Assignment (=), Compound Assignment (e.g., +=, -=, *=, /=, %=)]
+function assignment_operators() {
+    // assignment operator
+    let a = 7;
+    console.log("Assignment: a = 7, a =", a);
+    // addition assignment operator
+    a += 5;  // a = a + 5
+    console.log("Addition Assignment: a += 5, a =", a);
+    // subtraction assignment operator
+    a -= 5;  // a = a - 5
+    console.log("Subtraction Assignment: a -= 5, a =", a);
+    // multiplication assignment operator
+    a *= 2;  // a = a * 2
+    console.log("Multiplication Assignment: a *= 2, a =", a);
+    // division assignment operator
+    a /= 2;  // a = a / 2
+    console.log("Division Assignment: a /= 2, a =", a);
+    // remainder assignment operator
+    a %= 2;  // a = a % 2
+    console.log("Remainder Assignment: a %= 2, a =", a);
+    // exponentiation assignment operator
+    a **= 2;  // a = a**2
+    console.log("Exponentiation Assignment: a **= 7, a =", a);
+}
+// assignment_operators();
+
+/* -------------------------------------------------------------------------- */
+function comparision_operator() {
+    // equal to operator
+    console.log("Equal to: 2 == 2 is", 2 == 2);
+    // not equal operator
+    console.log("Not equal to: 3 != 3 is", 3 != 3);
+    // strictly equal to operator
+    console.log("Strictly equal to: 2 === '2' is", 2 === '2');
+    // strictly not equal to operator
+    console.log("Strictly not equal to: 2 !== '2' is", 2 !== '2');
+    // greater than operator
+    console.log("Greater than: 3 > 3 is", 3 > 3);
+    // less than operator
+    console.log("Less than: 2 > 2 is", 2 > 2);
+    // greater than or equal to operator
+    console.log("Greater than or equal to: 3 >= 3 is", 3 >= 3);
+    // less than or equal to operator
+    console.log("Less than or equal to: 2 <= 2 is", 2 <= 2);
+}
+// comparision_operator();
+/* -------------------------------------------------------------------------- */
