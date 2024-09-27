c01a1355099f09b6017416104073f50a95240838
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-09-27 18:34:12 +0530

    Update 27-sep-24.js

--- JS Challenges/27-sep-24.js
+++ JS Challenges/27-sep-24.js
@@ -187,3 +187,24 @@ function Book(title, author) {
 }
 
 // Instantiate your Book constructor here
+/* -------------------------------------------------------------------------- */
+
+// Coaxial Cable Impedance
+// Create a function that takes the values Dd (Dielectric Outer Diameter), Dc (Inner Conductor Diameter) and er (Dielectric Constant) and calculates the Coaxial Cable Impedance.
+
+// Examples
+// impedanceCalculator(20.7, 2, 4) ➞ 70.0
+
+// impedanceCalculator(5.3, 1.2, 2.2) ➞ 60.0
+
+// impedanceCalculator(4.48, 1.33, 2.2) ➞ 50.0
+// Notes
+// If you get stuck on a challenge, find help in the Resources tab.
+// Round your result to one decimal place.
+
+function impedanceCalculator(Dd, Dc, er) {
+    console.log(Math.round(138 * Math.log10(Dd / Dc) / Math.sqrt(er)));
+}
+// impedanceCalculator(20.7, 2, 4);
+/* -------------------------------------------------------------------------- */
+
