8f984e61c86714a64f49cef0cb7ed2978c7184bd
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-09-27 20:15:57 +0530

    Update 27-sep-24.js

--- JS Challenges/27-sep-24.js
+++ JS Challenges/27-sep-24.js
@@ -232,3 +232,29 @@ function expensiveOrders(orders, cost) {
     console.log(obj);
 }
 // expensiveOrders({ "a": 3000, "b": 200, "c": 1050 }, 1000);
+/* -------------------------------------------------------------------------- */
+
+// Scalable Mountain?
+// Given an array of numbers, representing the height of a mountain in certain intervals, return whether this mountain is scalable.
+
+// A mountain can be considered scalable if each number is within 5 units of the next number in either direction.
+
+// Examples
+// isScalable([1, 2, 4, 6, 7, 8]) ➞ true
+
+// isScalable([40, 45, 50, 45, 47, 52]) ➞ true
+
+// isScalable([2, 9, 11, 10, 18, 21]) ➞ false
+// Notes
+// The array may start at any number and can be any length.
+
+function isScalable(arr) {
+    let val = true;
+    for (let i = 1; i < arr.length; i++) {
+        if (!(arr[i] - arr[i - 1] <= 5 || arr[i] - arr[i - 1] <= -5)) {
+            val = false;
+        }
+    }
+    console.log(val);
+}
+isScalable([40, 45, 50, 45, 47, 52]);
