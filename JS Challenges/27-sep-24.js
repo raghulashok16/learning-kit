06cd16d23335e5518dc12c4741fa48385a2654aa
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-09-27 18:37:09 +0530

    Update 27-sep-24.js

--- JS Challenges/27-sep-24.js
+++ JS Challenges/27-sep-24.js
@@ -208,3 +208,20 @@ function impedanceCalculator(Dd, Dc, er) {
 // impedanceCalculator(20.7, 2, 4);
 /* -------------------------------------------------------------------------- */
 
+// Expensive Orders
+// Write a function that has two parameters: orders and cost. Return any orders that are greater than the cost.
+
+// Examples
+// expensiveOrders({ "a": 3000, "b": 200, "c": 1050 }, 1000)
+// ➞ { "a": 3000, "c": 1050 }
+
+// expensiveOrders({ "Gucci Fur": 24600, "Teak Dining Table": 3200, "Louis Vutton Bag": 5550, "Dolce Gabana Heels": 4000 }, 20000)
+// ➞ { "Gucci Fur": 24600 }
+
+// expensiveOrders({ "Deluxe Burger": 35, "Icecream Shake": 4, "Fries": 5 }, 40)
+// ➞ {}
+
+function expensiveOrders(orders, cost) {
+
+}
+expensiveOrders({ "Gucci Fur": 24600, "Teak Dining Table": 3200, "Louis Vutton Bag": 5550, "Dolce Gabana Heels": 4000 }, 20000);
