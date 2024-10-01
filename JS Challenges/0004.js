6afb3fdfc36415be64252ec81030acb88159a954
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-10-01 19:22:39 +0530

    Update 0004.js

--- JS Challenges/0004.js
+++ JS Challenges/0004.js
@@ -105,3 +105,91 @@ function superheroes(heroes) {
     }
 }
 // superheroes(["Batman", "Superman", "Spider-man", "Hulk", "Wolverine", "Wonder-Woman"])
+
+/* -------------------------------------------------------------------------- */
+// Read the Abacus (Part 1: Russian Schoty)
+// The Russian schoty is type of abacus (counting tool) that is used by sliding threaded beads along horizontal wires. An example schoty would have 7 wires, each holding 10 beads. Each bead, when moved to the left, would count as 1 unit. Starting from the bottom wire and moving up, the units increase by a factor of 10. If we use "O" for a bead and "-" to show the wire, we can represent the schoty as follows:
+
+// ---OOOOOOOOOO  millions
+// ---OOOOOOOOOO  hundred-thousands
+// ---OOOOOOOOOO  ten-thousands
+// ---OOOOOOOOOO  thousands
+// ---OOOOOOOOOO  hundreds
+// ---OOOOOOOOOO  tens 
+// ---OOOOOOOOOO  ones
+// To read the number, we count the beads on the left-hand side of each wire. In the example below, the number is 501264:
+
+// ---OOOOOOOOOO  0
+// OOOOO---OOOOO  5
+// ---OOOOOOOOOO  0
+// O---OOOOOOOOO  1
+// OO---OOOOOOOO  2
+// OOOOOO---OOOO  6
+// OOOO---OOOOOO  4
+// Given an array of strings representing each wire in the schoty, return the number being displayed.
+
+// Examples
+// schoty([
+//   "---OOOOOOOOOO",
+//   "---OOOOOOOOOO",
+//   "---OOOOOOOOOO",
+//   "OOO---OOOOOOO",
+//   "O---OOOOOOOOO",
+//   "OOOOOOOOO---O",
+//   "OO---OOOOOOOO"
+// ]) ➞ 3192
+
+// schoty([
+//   "OO---OOOOOOOO",
+//   "O---OOOOOOOOO",
+//   "OOOOO---OOOOO",
+//   "---OOOOOOOOOO",
+//   "---OOOOOOOOOO",
+//   "---OOOOOOOOOO",
+//   "---OOOOOOOOOO"
+// ]) ➞ 2150000
+// Notes
+// For more info on how to use a schoty, please check the Resources tab. This challenge ignores the traditional 4-bead wires used for quarter-ruble and quarter-kopek calculations.
+
+function schoty(frame) {
+    let arr = [];
+    for (let i = 0; i < frame.length; i++) {
+        let arrVal = frame[i].split('---')[0];
+        arr.push(arrVal.length);
+    }
+    console.log(Number(arr.join('')));
+}
+// schoty([
+//     "OO---OOOOOOOO",
+//     "O---OOOOOOOOO",
+//     "OOOOO---OOOOO",
+//     "---OOOOOOOOOO",
+//     "---OOOOOOOOOO",
+//     "---OOOOOOOOOO",
+//     "---OOOOOOOOOO"
+// ])
+
+/* -------------------------------------------------------------------------- */
+// Check if the String is a Palindrome
+// A palindrome is a word, phrase, number or other sequence of characters which reads the same backward or forward, such as madam or kayak.
+
+// Write a function that takes a string and determines whether it's a palindrome or not. The function should return a boolean (true or false value).
+
+// Examples
+// isPalindrome("Neuquen") ➞ true
+
+// isPalindrome("Not a palindrome") ➞ false
+
+// isPalindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!") ➞ true
+// Notes
+// Should be case insensitive.
+// Special characters (punctuation or spaces) should be ignored.
+
+function isPalindrome(str) {
+    const regex = /[^a-zA-Z0-9\s]/g;
+    var transformedString = str.toLowerCase().replace(regex, '').replace(/\s+/g, '');
+    let revStr = str.toLowerCase().split('').reverse().join('').replace(regex, '').replace(/\s+/g, '');
+    console.log(revStr === transformedString);
+}
+// isPalindrome("Not a palindrome");
+isPalindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!");
