a8518dc0348cc476fff009ff1cb66dbd600b0cf7
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-10-04 18:06:56 +0530

    Update 0006.js

--- JS Challenges/0006.js
+++ JS Challenges/0006.js
@@ -21,6 +21,128 @@
 // Bonus: Can you write a solution that achieves this by only traversing the array once? (i.e. without finding the max beforehand)
 
 function incrementToTop(arr) {
-
+    let maxVal = Math.max(...arr);
+    console.log(maxVal);
+    let steps = 0;
+    for (const val of arr) {
+        steps = steps + (maxVal - val);
+    }
+    console.log(steps)
 }
-incrementToTop([3, 10, 3])
+// incrementToTop([3, 4, 5]);
+
+/* -------------------------------------------------------------------------- */
+// Lonely Integer
+// You are given an array of integers having both negative and positive values, except for one integer which can be negative or positive. Create a function to find out that integer.
+
+// Examples
+// lonelyInteger([1, -1, 2, -2, 3]) ➞ 3
+// // 3 has no matching negative appearance.
+
+// lonelyInteger([-3, 1, 2, 3, -1, -4, -2]) ➞ -4
+// // -4 has no matching positive appearance.
+
+// lonelyInteger([-9, -105, -9, -9, -9, -9, 105]) ➞ -9
+
+function lonelyInteger(arr) {
+    let arr2 = arr.filter(v => v > 0);
+    let arr3 = arr.filter(v => v < 0);
+    console.log(arr2);
+    console.log(arr3);
+    let arr4 = (arr2.length > arr3.length) ? arr2 : arr3;
+    for (const val of arr4) {
+        if (!arr.includes(val * -1)) {
+            console.log(val);
+        }
+    }
+}
+// lonelyInteger([-3, 1, 2, 3, -1, -4, -2]);
+
+/* -------------------------------------------------------------------------- */
+// Perfect Square Patch
+// Create a function that takes an integer and outputs an n x n square solely consisting of the integer n.
+
+// Examples
+// squarePatch(3) ➞ [
+//   [3, 3, 3],
+//   [3, 3, 3],
+//   [3, 3, 3]
+// ]
+
+// squarePatch(5) ➞ [
+//   [5, 5, 5, 5, 5],
+//   [5, 5, 5, 5, 5],
+//   [5, 5, 5, 5, 5],
+//   [5, 5, 5, 5, 5],
+//   [5, 5, 5, 5, 5]
+// ]
+
+// squarePatch(1) ➞ [
+//   [1]
+// ]
+
+// squarePatch(0) ➞ []
+
+function squarePatch(n) {
+    let temp = [];
+    for (let i = 0; i < n; i++) {
+        let temp1 = [];
+        for (let j = 0; j < n; j++) {
+            temp1.push(n);
+        }
+        temp.push(temp1);
+    }
+    console.log(temp);
+}
+// squarePatch(3);
+
+/* -------------------------------------------------------------------------- */
+// Swap the Numbers
+// Imagine you have three numbers: a, b, and c. c is equal to either a or b, but you don't know which one. Your task is to create a function that returns whatever number c isn't, out of a and b. So, if c is equal to a, return b, and if c is equal to b, return a. Here's what makes this challenge difficult: you cannot use any if statements.
+
+// Examples
+// swap(1, 0, 0) ➞ 1
+// // a = 1, b = 0, c = b
+// // return a
+
+// swap(1, 3, 1) ➞ 3
+// // a = 1, b = 3, c = a
+// // return b
+
+// swap(27, 31, 31) ➞ 27
+// // a = 27, b = 31, c = b
+// // return a
+// Notes
+// To prevent cheating, you also can't call any functions.
+// c will always be equal to either a or b.
+// a will never equal b.
+// a, b, and c will always be integers.
+
+function swap(a, b, c) {
+    console.log((b === c) ? a : b);
+}
+// swap(27, 31, 31);
+
+/* -------------------------------------------------------------------------- */
+// Doubled Pay
+// An employee working at a very bizarre company earns one penny on their first day. However, for every day that passes, their base amount doubles, so they earn two pennies on the second day and four pennies on the third day (totalling 7 pennies). Given a number of days, return how many pennies the employee accumulates.
+
+// Examples
+// doubledPay(1) ➞ 1
+
+// doubledPay(2) ➞ 3
+
+// doubledPay(3) ➞ 7
+// Notes
+// You will only get tests for valid positive integers.
+
+function doubledPay(n) {
+    let temp = 0;
+    let tempValue = 1;
+    for (let i = 1; i <= n; i++) {
+        temp = temp + tempValue
+        tempValue = tempValue * 2;
+    }
+    console.log(temp);
+}
+// doubledPay(4);
