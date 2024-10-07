c73abbc4f221d0a8bd1e074f9d38944ea25456c5
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-10-07 22:28:06 +0530

    Update 0006.js

--- JS Challenges/0006.js
+++ JS Challenges/0006.js
@@ -146,3 +146,34 @@ function doubledPay(n) {
     console.log(temp);
 }
 // doubledPay(4);
+
+/* -------------------------------------------------------------------------- */
+// Nth Smallest Integer
+// Given an unsorted array, create a function that returns the nth smallest integer (the smallest integer is the first smallest, the second smallest integer is the second smallest, etc).
+
+// Examples
+// nthSmallest([1, 3, 5, 7], 1) ➞ 1
+
+// nthSmallest([1, 3, 5, 7], 3) ➞ 5
+
+// nthSmallest([1, 3, 5, 7], 5) ➞ null
+
+// nthSmallest([7, 3, 5, 1], 2) ➞ 3
+// Notes
+// n will always be >= 1.
+// Each integer in the array will be distinct (there will be a clear ordering).
+// Given an out of bounds parameter (e.g. an array is of size k), and you are asked to find the m > k smallest integer, return null.
+
+function nthSmallest(arr, n) {
+    let temp = [...arr];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (temp[j] < temp[j + 1]) {
+                [temp[j], temp[j + 1]] = [temp[j + 1], temp[i]];
+            }
+        }
+    }
+    console.log(temp);
+    console.log(temp[n - 1] === undefined ? null : temp[n - 1]);
+}
+// nthSmallest([1, 3, 5, 7], 5);
