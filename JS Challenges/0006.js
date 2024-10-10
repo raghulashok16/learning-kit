d77dba00652e78f5f17ffe152cc5a02578db3e81
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-10-10 17:56:15 +0530

    Update 0006.js

--- JS Challenges/0006.js
+++ JS Challenges/0006.js
@@ -177,3 +177,22 @@ function nthSmallest(arr, n) {
     console.log(temp[n - 1] === undefined ? null : temp[n - 1]);
 }
 // nthSmallest([1, 3, 5, 7], 5);
+
+/* -------------------------------------------------------------------------- */
+// Find the Odd Integer
+// Create a function that takes an array and finds the integer which appears an odd number of times.
+
+// Examples
+// findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]) ➞ -1
+
+// findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]) ➞ 5
+
+// findOdd([10]) ➞ 10
+// Notes
+// There will always only be one integer that appears an odd number of times.
+
+function findOdd(arr) {
+
+}
+
+findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]);
